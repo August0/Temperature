2d5499ddcf9d26236e1d462274489b9be6e4227c
Fabio Z Tessitore <fabioztessitore@gmail.com>
2015-12-16 13:15:40 +0100

    codice base

---
+++ fahr_cels.c
@@ -0,0 +1,27 @@
+/* fahr_cels.c */
+
+/* stampa una tabella di conversione
+ * da gradi fahrenheit a celsius
+ */
+
+#include <stdio.h>
+
+int main()
+{
+  float cels, fahr;
+  int lower, upper, step;
+
+  lower = 0;
+  upper = 300;
+  step = 20;
+
+  fahr = lower;
+  while (fahr <= upper) {
+    cels = (5./9.) * (fahr-32.);
+    printf(" %3.0f %6.1f\n", fahr, cels);
+
+    fahr = fahr + step;
+  }
+
+  return 0;
+}
