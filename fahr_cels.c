454340a4d671337a4f978b1628ff5c9a8428f375
August0 <augustofappio@gmail.com>
2015-12-17 19:37:58 +0100

    Modifiche effettuate

--- fahr_cels.c
+++ fahr_cels.c
@@ -10,11 +10,11 @@ int main()
 {
   float cels, fahr;
   int lower, upper, step;
-
+ printf("Questa tabella converte i gradi celsius in fahrenheit\n");
   lower = 0;
   upper = 300;
   step = 20;
-
+  
   fahr = lower;
   while (fahr <= upper) {
     cels = (5./9.) * (fahr-32.);
